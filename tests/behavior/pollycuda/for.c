15cb0b46a523562106a912493d33d07e0e778560
Dmitry Mikushin <dmitry@kernelgen.org>
2012-02-16 17:44:38 +0000

    New version of for.c from NN

--- tests/behavior/pollycuda/for.c
+++ tests/behavior/pollycuda/for.c
@@ -1,12 +1,19 @@
-#define N 10
-
+#include <stdlib.h>
+#include <stdio.h>
 int main(int argc, char ** argv)
 {
-   int matrix[N][N];
+   int a = atoi(argv[1]);
+   int b = atoi(argv[2]);
+   int c = atoi(argv[3]);
+   //char * matrix = (char*)malloc(a*b*sizeof(char));
+   char * matrix = (char*)malloc((a)*(b + a + c)*sizeof(char));
+   //int matrix[a*b*sizeof(int)];
    int i,j;
-   for(i = 0; i < N; i++)
-       for(j = 0; j < N; j++)
-           matrix[i][j] = i+j+200;
-   return matrix[0][0];
+   for(i = 0; i < a; i++)
+       for(j = 0; j < b + c + i; j++)
+           matrix[i*(b + c + a) + j] = i+j;
+   FILE * file = fopen("result.txt","w");
+   fprintf(file, "%d %d %d", matrix[0], matrix[1],matrix[2]);
+   return 0;
 }
-//89099883148
+//89099883148x
