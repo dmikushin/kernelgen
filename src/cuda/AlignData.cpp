58cfa33fdd0b45042f84831efa6be441d24d7aed
Dmitry Mikushin <dmitry@kernelgen.org>
2013-10-17 00:54:12 +0000

    Align GPU constant memory variables to the page boundary
    Summary:
    Last LLVM catchup added NVPTX support for emission of GPU constant memory variables from constant arrays (previously they were mapped to the global memory). These new constant memory variables landed to the binary unaligned, because currently we align only global and initialized global sections. As result, sincos_sp/sincos_dp tests were broken.
    
    This patch adds aligning of constant memory variables. The change unbreaks sincos_sp test, while sincos_dp is still broken due to some other reason.
    
    Test Plan:
    Testing showed sincos_sp results for runmode=1 perfectly match the host version:
    
      marcusmae@M17xR4:~/forge/kernelgen/tests/behavior/sincos$ ./sincos_sp 16 16 16
         2055.32373      0.999993563       4.68776561E-05
         2034.03088      0.999500394       8.03829171E-05
         5341.61621       1.83881903      0.552197516    
         2055.32373      0.999993563       4.68776561E-05
         2034.03088      0.999500394       8.03829171E-05
         5341.61621       1.83881903      0.552197516    
         1682.68640      0.999993563       4.68776561E-05
         1677.36230      0.999500394       5.22442628E-04
         4390.95410       1.83881903      0.552197516    
      marcusmae@M17xR4:~/forge/kernelgen/tests/behavior/sincos$ kernelgen_runmode=1 ./sincos_sp 16 16 16
         2055.32373      0.999993563       4.68776561E-05
         2034.03088      0.999500394       8.03829171E-05
      Launching kernel __kernelgen_sincos__loop_97
          blockDim = { 128, 1, 1 }
          gridDim = { 1, 16, 16 }
      Finishing kernel __kernelgen_sincos__loop_97
         5341.61621       1.83881903      0.552197516    
         2055.32373      0.999993563       4.68776561E-05
         2034.03088      0.999500394       8.03829171E-05
      Launching kernel __kernelgen_sincos__loop_97
          blockDim = { 128, 1, 1 }
          gridDim = { 1, 16, 16 }
      Finishing kernel __kernelgen_sincos__loop_97
         5341.61621       1.83881903      0.552197516    
         1682.68640      0.999993563       4.68776561E-05
         1677.36230      0.999500394       5.22442628E-04
      Launching kernel __kernelgen_sincos__loop_97
          blockDim = { 128, 1, 1 }
          gridDim = { 1, 15, 15 }
      Finishing kernel __kernelgen_sincos__loop_97
         4390.95410       1.83881903      0.552197516 
    
    
    
    Reviewers: administrator
    
    Reviewed By: administrator
    
    Differential Revision: http://devel.kernelgen.org/D4

--- src/cuda/AlignData.cpp
+++ src/cuda/AlignData.cpp
@@ -53,8 +53,8 @@ void kernelgen::bind::cuda::CUBIN::AlignData(const char *cubin, size_t align) {
 
     //
     // 2) Find ELF section containing the symbol table and
-    // load its data. Meanwhile, search for the global and initialized
-    // global sections, count the total number of sections.
+    // load its data. Meanwhile, search for the constant, global and
+    // initialized global sections.
     //
     size_t shstrndx;
     if (elf_getshdrstrndx(e, &shstrndx)) {
@@ -65,9 +65,9 @@ void kernelgen::bind::cuda::CUBIN::AlignData(const char *cubin, size_t align) {
     Elf_Data *symbols = NULL;
     int nsymbols = 0, nsections = 0, link = 0;
     Elf_Scn *scn = elf_nextscn(e, NULL);
-    Elf_Scn *sglobal, *sglobal_init = NULL;
-    GElf_Shdr shglobal, shglobal_init;
-    int iglobal = -1, iglobal_init = -1;
+    Elf_Scn *sconst, *sglobal, *sglobal_init = NULL;
+    GElf_Shdr shconst, shglobal, shglobal_init;
+    int iconst = -1, iglobal = -1, iglobal_init = -1;
     for (int i = 1; scn != NULL; scn = elf_nextscn(e, scn), i++, nsections++) {
       GElf_Shdr shdr;
 
@@ -93,12 +93,16 @@ void kernelgen::bind::cuda::CUBIN::AlignData(const char *cubin, size_t align) {
       if ((name = elf_strptr(e, shstrndx, shdr.sh_name)) == NULL)
         throw;
 
-      if (!strcmp(name, ".nv.global")) {
+      // Two variants for different CC: sm_2x or sm_3x.
+      if (!strcmp(name, ".nv.constant2") || !strcmp(name, ".nv.constant3")) {
+        iconst = i;
+        sconst = scn;
+        shconst = shdr;
+      } else if (!strcmp(name, ".nv.global")) {
         iglobal = i;
         sglobal = scn;
         shglobal = shdr;
-      }
-      if (!strcmp(name, ".nv.global.init")) {
+      } else if (!strcmp(name, ".nv.global.init")) {
         iglobal_init = i;
         sglobal_init = scn;
         shglobal_init = shdr;
@@ -109,16 +113,22 @@ void kernelgen::bind::cuda::CUBIN::AlignData(const char *cubin, size_t align) {
       return;
 
     //
-    // 3) Count what size is needed to keep initialized
-    // global symbols, if they were aligned. Also update the sizes
-    // and offsets for individual initialized symbols.
+    // 3) Count what size is needed to keep constant, global,
+    // and initialized global symbols, if they were aligned.
+    // Also update the sizes and offsets for individual initialized symbols.
     //
+    vector<char>::size_type szconst_new = 0;
     vector<char>::size_type szglobal_new = 0, szglobal_init_new = 0;
     for (int isymbol = 0; isymbol < nsymbols; isymbol++) {
       GElf_Sym symbol;
       gelf_getsym(symbols, isymbol, &symbol);
 
-      if (symbol.st_shndx == iglobal) {
+      if (symbol.st_shndx == iconst) {
+        size_t szaligned = symbol.st_size;
+        if (szaligned % align)
+          szaligned += align - szaligned % align;
+        szconst_new += szaligned;
+      } else if (symbol.st_shndx == iglobal) {
         symbol.st_value = szglobal_new;
         if (symbol.st_size % align)
           symbol.st_size += align - symbol.st_size % align;
@@ -138,7 +148,56 @@ void kernelgen::bind::cuda::CUBIN::AlignData(const char *cubin, size_t align) {
     }
 
     //
-    // 4) Add new data for aligned globals section.
+    // 4) Add new data for aligned constants section.
+    //
+    if (iconst != -1) {
+      vector<char> vconst_new;
+      vconst_new.resize(szconst_new);
+
+      Elf_Data *data = elf_getdata(sconst, NULL);
+      if (!data) {
+        fprintf(stderr, "elf_newdata() failed: %s\n", elf_errmsg(-1));
+        throw;
+      }
+      char *dconst = (char *)data->d_buf;
+
+      char *pconst = (char *)dconst;
+      char *pconst_new = (char *)&vconst_new[0];
+      memset(pconst_new, 0, szconst_new);
+      szconst_new = 0;
+      for (int isymbol = 0; isymbol < nsymbols; isymbol++) {
+        GElf_Sym symbol;
+        gelf_getsym(symbols, isymbol, &symbol);
+
+        if (symbol.st_shndx == iconst) {
+          memcpy(pconst_new, pconst + symbol.st_value,
+                 symbol.st_size);
+
+          symbol.st_value = szconst_new;
+          if (symbol.st_size % align)
+            symbol.st_size += align - symbol.st_size % align;
+          szconst_new += symbol.st_size;
+          pconst_new += symbol.st_size;
+
+          if (!gelf_update_sym(symbols, isymbol, &symbol)) {
+            fprintf(stderr, "gelf_update_sym() failed for %s: %s\n", cubin,
+                    elf_errmsg(-1));
+            throw;
+          }
+        }
+      }
+
+      data->d_buf = (char *)&vconst_new[0];
+      data->d_size = szconst_new;
+
+      if (!gelf_update_shdr(sconst, &shconst)) {
+        fprintf(stderr, "gelf_update_shdr() failed: %s\n", elf_errmsg(-1));
+        throw;
+      }
+    }
+
+    //
+    // 5) Add new data for aligned globals section.
     //
     if (iglobal != -1) {
       vector<char> vglobal_new;
@@ -160,10 +219,10 @@ void kernelgen::bind::cuda::CUBIN::AlignData(const char *cubin, size_t align) {
     }
 
     //
-    // 5) Add new data for aligned initialized globals section.
+    // 6) Add new data for aligned initialized globals section.
     //
-    vector<char> vglobal_init_new;
     if (iglobal_init != -1) {
+      vector<char> vglobal_init_new;
       vglobal_init_new.resize(szglobal_init_new);
 
       Elf_Data *data = elf_getdata(sglobal_init, NULL);
@@ -209,7 +268,7 @@ void kernelgen::bind::cuda::CUBIN::AlignData(const char *cubin, size_t align) {
     }
 
     //
-    // 6) Commit changes into the underlying ELF binary.
+    // 7) Commit changes into the underlying ELF binary.
     //
     if (elf_update(e, ELF_C_WRITE) == -1) {
       fprintf(stderr, "Cannot update the ELF image %s\n", cubin);
